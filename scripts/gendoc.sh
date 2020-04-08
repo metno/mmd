faf76780c711d2e2c6df1a2b32e8e48cae5b3a0f
magnarem <magnarem@met.no>
2020-04-08 11:44:39 +0200

    Removed unused scripts folder and dummy script

--- scripts/gendoc.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-# Generate asciidoctor documentation
-# Need to be run from same dir as script.
-asciidoctor -B ../doc/ ../doc/mmd-specification.adoc
-asciidoctor-pdf -B ../doc/ -D ../doc/ ../doc/mmd-specification.adoc 
