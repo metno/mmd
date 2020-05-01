59f6fe3711013b0aab101a802049d2cf4a56ff1f
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2020-05-01 15:48:25 +0200

    #4: moved some code into push.sh

---
+++ push.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+setup_git() {
+    git config --global user.email $GIT_EMAIL
+    git config --global user.name $GIT_NAME
+}
+
+commit_files() {
+    # We work directly on master. Builds go to the doc-folder.
+    git add doc/mmd-specification.html
+    git add doc/mmd-specification.pdf
+    #git commit -m "Auto-generated from ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}"
+    git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
+}
+
+upload_files(){
+    git push https://${GH_TOKEN}@github.com/mortenwh/mmd.git > /dev/null 2>&1
+}
