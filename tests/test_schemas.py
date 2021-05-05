ba4331d5dc7adf17714f3a47c0479a4da09c3825
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-05-05 12:48:00 +0200

    #153: unittest checking that mmd.xsd can be opened as an xml schema

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,6 +1,7 @@
 import os
-import unittest
+import lxml
 import pathlib
+import unittest
 
 import lxml.etree as ET
 
@@ -9,5 +10,4 @@ class TestXSDs(unittest.TestCase):
     def test_mmd_xsd(self):
         xsd_schema = os.path.join(pathlib.Path.cwd(), 'xsd', 'mmd.xsd')
         xmlschema_mmd = ET.XMLSchema(ET.parse(xsd_schema))
-        import ipdb
-        ipdb.set_trace()
+        self.assertIsInstance(xmlschema_mmd, lxml.etree.XMLSchema)
