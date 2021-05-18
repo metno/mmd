ccebb639d336b9697086226f57e2c75a044200c6
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-05-18 09:48:31 +0200

    #157: added test on mmd_strict schema

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -11,3 +11,8 @@ class TestXSDs(unittest.TestCase):
         xsd_schema = os.path.join(pathlib.Path.cwd(), 'xsd', 'mmd.xsd')
         xmlschema_mmd = ET.XMLSchema(ET.parse(xsd_schema))
         self.assertIsInstance(xmlschema_mmd, lxml.etree.XMLSchema)
+
+    def test_mmd_xsd_strict(self):
+        xsd_schema = os.path.join(pathlib.Path.cwd(), 'xsd', 'mmd_strict.xsd')
+        xmlschema_mmd = ET.XMLSchema(ET.parse(xsd_schema))
+        self.assertIsInstance(xmlschema_mmd, lxml.etree.XMLSchema)
