5fc7a1b5afae9caca035491f51a2403ae430f5a7
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-09-03 19:03:23 +0200

    #162: Added tests

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -16,3 +16,12 @@ class TestXSDs(unittest.TestCase):
         xsd_schema = os.path.join(pathlib.Path.cwd(), 'xsd', 'mmd_strict.xsd')
         xmlschema_mmd = ET.XMLSchema(ET.parse(xsd_schema))
         self.assertIsInstance(xmlschema_mmd, lxml.etree.XMLSchema)
+
+class TestXSLTs(unittest.TestCase):
+
+    def test_dcatap_xslt_instance(self):
+        transform_to_dcat = ET.XSLT(ET.parse('xslt/mmd-to-dcatap.xsl'))
+        self.assertIsInstance(transform_to_dcat, lxml.etree.XSLT)
+
+    def test_mmd_to_dcatap_translation(self):
+        intentionally failing - please use an example MMD file and check that it is translated
