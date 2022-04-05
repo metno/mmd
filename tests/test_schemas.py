0277ebdc3abd3698b69f46bbc13a0012dd1856ea
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2022-04-05 12:23:44 +0200

    #185: use='required' but it doesn't seem to work for xml:lang...

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -36,6 +36,20 @@ class TestXSDs(unittest.TestCase):
         self.assertFalse(valid1)
         self.assertFalse(valid2)
 
+    def test_mmd_xsd_strict_fails_on_missing_lang_attr(self):
+        mmd_schema = ET.ElementTree(
+                file = os.path.join(
+                    pathlib.Path.cwd(),
+                    'tests',
+                    'data',
+                    'precipitation_amount_st_92350_NO_LANG_ATTR.xml'
+                )
+            )
+        xsd_schema1 = os.path.join(pathlib.Path.cwd(), 'xsd', 'mmd_strict.xsd')
+        xsd_obj1 = ET.XMLSchema(ET.parse(xsd_schema1))
+        valid1 = xsd_obj1.validate(mmd_schema)
+        self.assertFalse(valid1)
+
     #def test_mmd_xsd_passing(self):
     #    mmd_schema = ET.ElementTree(
     #            file = os.path.join(
