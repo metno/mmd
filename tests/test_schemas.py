9a00b73553145f5c7c5a65736993d93145e63c13
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2022-10-27 10:36:12 +0200

    #201: updated path in test

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -80,11 +80,11 @@ class TestXSDs(unittest.TestCase):
 
 class TestXSLTs(unittest.TestCase):
 
-    def test_mmd_to_geonorge_xslt(self):
-        translator = ET.XSLT(ET.parse('xslt/mmd-to-geonorge.xslt'))
+    def test_mmd_to_geonorge_xsl(self):
+        translator = ET.XSLT(ET.parse('xslt/mmd-to-geonorge.xsl'))
         self.assertIsInstance(translator, lxml.etree.XSLT)
         
-    def test_dcatap_xslt_instance(self):
+    def test_dcatap_xsl_instance(self):
         transform_to_dcat = ET.XSLT(ET.parse('xslt/mmd-to-dcatap.xsl'))
         self.assertIsInstance(transform_to_dcat, lxml.etree.XSLT)
 
